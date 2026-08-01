7d59de46faf29ba58bd598df59cc106e19eef4a9
takashit16833 <takashit16833@gmail.com>
2026-08-01 07:06:51 +0000

    EventSpec作成

--- ragscope-app/logging-src/RAGScope/Logging/EventSpec.hs
+++ ragscope-app/logging-src/RAGScope/Logging/EventSpec.hs
@@ -1,1 +1,44 @@
-module RAGScope.Logging.EventSpec where
+-- | 機能固有の閉じたログイベントを、共通EventSpecへ変換するためのAPI
+--
+-- 通常の機能処理ではなく、RAGScope.*.Loggingモジュールから利用する
+module RAGScope.Logging.EventSpec (
+  ErrorCode (..),
+  EventName (..),
+  EventSpec,
+  FieldName (..),
+  LogError,
+  LogErrorCategory (..),
+  LogValue (..),
+  OperationName (..),
+  Payload,
+  SafeMessage (..),
+  ToEventSpec (..),
+  debugEventSpec,
+  emptyPayload,
+  failedEventSpec,
+  infoEventSpec,
+  logError,
+  payloadFromList,
+  warnEventSpec,
+) where
+
+import RAGScope.Logging.Core (
+  ErrorCode (..),
+  EventName (..),
+  EventSpec,
+  FieldName (..),
+  LogError,
+  LogErrorCategory (..),
+  LogValue (..),
+  OperationName (..),
+  Payload,
+  SafeMessage (..),
+  ToEventSpec (..),
+  debugEventSpec,
+  emptyPayload,
+  failedEventSpec,
+  infoEventSpec,
+  logError,
+  payloadFromList,
+  warnEventSpec,
+ )
