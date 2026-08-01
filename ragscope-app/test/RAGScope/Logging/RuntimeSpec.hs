2c4f55d4693442641fd09009ad7a8b6e0ec82ad7
takashit16833 <takashit16833@gmail.com>
2026-08-01 23:17:11 +0000

    4. emitの失敗とログ0件を区別する やってみた

--- ragscope-app/test/RAGScope/Logging/RuntimeSpec.hs
+++ ragscope-app/test/RAGScope/Logging/RuntimeSpec.hs
@@ -16,30 +16,29 @@ instance ToEventSpec TestEventType where
           (EventName "event")
           emptyPayload
 
-test :: IO [LogEvent]
-test = do
-  (memorySink, readEvents) <- newMemorySink
-
-  let logger =
-        mkLogger
-          Debug
-          RAGScopeApp
-          (ExecutionContext fixedExecutionId)
-          fixedEventIdSource
-          fixedClock
-          memorySink
-
-  result <- emit logger (TestEventType "debug")
-
-  case result of
-    Right () -> readEvents
-    Left _ -> pure []
-
 spec :: Spec
 spec = do
   describe "emit" $ do
     context "とりあえずなんかテスト" $ do
       it "EventIdのテスト" $ do
-        [logEvent] <- test :: IO [LogEvent]
-        let (EventId actualEventUuid) = logEvent.eventId
-        actualEventUuid `shouldBe` fixedEvenUuid
+        (memorySink, readEvents) <- newMemorySink
+
+        let logger =
+              mkLogger
+                Debug
+                RAGScopeApp
+                (ExecutionContext fixedExecutionId)
+                fixedEventIdSource
+                fixedClock
+                memorySink
+
+        result <- emit logger (TestEventType "debug")
+        logEvents <- readEvents
+
+        result `shouldBe` Right ()
+
+        length logEvents `shouldBe` 1
+
+        let (EventId actualId) = (head logEvents).eventId
+
+        actualId `shouldBe` fixedEvenUuid
